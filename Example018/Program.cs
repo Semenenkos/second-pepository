592529a5e5117db12d972ef7c8698cdd300a6562
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-14 23:03:33 +0300

    Add Task 2 from homework to seminar 1

---
+++ Example018/Program.cs
@@ -0,0 +1,17 @@
+﻿//Домашнее задание. Семинар 1. Задача 2.
+
+string s_a = Console.ReadLine();
+string s_b = Console.ReadLine();
+string s_c = Console.ReadLine();
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+int c = int.Parse(s_c);
+if (a < b)
+{
+    a = b;
+}
+if (a < c)
+{
+    a = c;
+}
+Console.Write($"{a} is a largest ");
