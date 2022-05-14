4447a02ef1674b3985ab227f75bd99583ce617d1
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-14 21:11:43 +0300

    Add Parse and a*a. Part 2

---
+++ Example013/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа, принимающая на вход два числа и дающая проверить: является ли первое число, квадратом второго.
+
+string s_a = Console.ReadLine();
+string s_b = Console.ReadLine();
+int a = int.Parse(s_a); 
+int b = int.Parse(s_b);
+if (b == a * a)
+{
+    Console.WriteLine ("Yes");
+}
+else
+{
+    Console.WriteLine ("No");
+}
