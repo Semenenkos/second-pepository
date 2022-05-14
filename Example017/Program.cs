e73947fe37ca3a64eefc8f5c542d5911ddad070a
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-14 22:43:42 +0300

    Add Task 1 from homework to seminar 1

---
+++ Example017/Program.cs
@@ -0,0 +1,18 @@
+﻿//Домашнее задание. Семинар 1. Задача 1.
+
+string s_a = Console.ReadLine();
+string s_b = Console.ReadLine();
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+if (a > b)
+{
+    Console.Write ($"{a} is greater than {b} ");
+}
+else if (a == b)
+{
+    Console.Write ($"{a} is equal to {b} ");
+}
+else
+{
+    Console.Write ($"{b} is greater than {a} ");
+}
