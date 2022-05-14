4e9aae77c4c13cde8022a9d3d06e846e6f1bc886
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-14 12:32:27 +0300

    Add special username

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "носик")
+{
+    Console.WriteLine("Ой, это кито? Это зи, мой любимый носик");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine (username);
+}
