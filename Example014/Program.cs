805cd4efd1c9af49e5cd2886372c5e90f2ee0a20
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-14 21:22:36 +0300

    Add Task 2 from seminar

---
+++ Example014/Program.cs
@@ -0,0 +1,30 @@
+﻿string first = Console.ReadLine();
+
+if (first == "1")
+{
+    Console.WriteLine ("M");
+}
+if (first == "2")
+{
+    Console.WriteLine ("TU");
+}
+if (first == "3")
+{
+    Console.WriteLine ("W");
+}
+if (first == "4")
+{
+    Console.WriteLine ("TH");
+}
+if (first == "5")
+{
+    Console.WriteLine ("F");
+}
+if (first == "6")
+{
+    Console.WriteLine ("SA");
+}
+if (first == "7")
+{
+    Console.WriteLine ("SU");
+}
