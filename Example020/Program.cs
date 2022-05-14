606d28b1b9512823ec7d3d21b87ff15563f5b798
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-14 23:23:42 +0300

    Add Task 4 from homework to seminar 1

---
+++ Example020/Program.cs
@@ -0,0 +1,12 @@
+﻿//Домашнее задание. Семинар 1. Задача 4.
+
+string s_a = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int i = 2;
+
+while (i <= a)
+{
+    Console.Write ($"{i} ");
+    i += 2;
+}
