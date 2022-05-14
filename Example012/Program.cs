c7f545cce8b4731b815e60c3bef7213980981136
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-14 20:41:53 +0300

    Add Parse and a*a

---
+++ Example012/Program.cs
@@ -0,0 +1,4 @@
+﻿string s_a = Console.ReadLine(); // s_a - переменная строки
+int a = int.Parse(s_a); // Преобразователь из строки "string" в "integer" - команда ".Parse"
+
+Console.WriteLine (a*a);
