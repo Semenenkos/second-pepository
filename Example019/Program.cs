3fee08ca62f80a6d26f358d4848cd795e91f6df7
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-14 23:15:35 +0300

    Add Task 3 from homework to seminar 1

---
+++ Example019/Program.cs
@@ -0,0 +1,14 @@
+﻿//Домашнее задание. Семинар 1. Задача 3.
+
+string s_a = Console.ReadLine();
+
+int a = int.Parse(s_a);
+
+if (a % 2 == 0)
+{
+    Console.Write($"{a} is even");
+}
+else
+{
+    Console.Write($"{a} is odd");
+}
