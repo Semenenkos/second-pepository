daa9afdea4a40b8c91ada8a25e1ad68a992aa2ab
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-05-17 20:39:39 +0300

    Add Info from seminar 3

---
+++ Example024/Program.cs
@@ -0,0 +1,20 @@
+﻿void Squares(int n)
+{
+    double[] nums = new double[n];
+    int index = 0;
+
+    Console.Write($"{n} -> ");
+
+    while (index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 2);
+        Console.Write($"{nums[index]}, ");
+        index += 1;
+    }    
+    Console.WriteLine();
+}
+
+Squares(6);
+Squares(2);
+Squares(11);
+
